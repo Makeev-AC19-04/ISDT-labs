d1ab29a9217ad4166435b2f0ff47e865d714787d
Makeev-AC19-04 <wane2001@gmail.com>
2022-10-02 16:54:27 +0300

    Добавьте файлы проекта.

---
+++ SocketClient/SocketClient.cpp
@@ -0,0 +1,109 @@
+// SocketClient.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../SocketServer/Message.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+void ProcessMessages()
+{
+	while (true)
+	{
+		Message m = Message::send(MR_BROKER, MT_GETDATA);
+		switch (m.header.type)
+		{
+		case MT_DATA:
+			cout << "New message: " << m.data << endl << "From: " << to_string(m.header.from) << endl;
+		default:
+			Sleep(100);
+			break;
+		}
+	}
+}
+
+
+void Client()
+{
+	AfxSocketInit();
+	cout << "Client has started\n";
+	thread t(ProcessMessages);
+	t.detach();
+
+	Message m = Message::send(MR_BROKER, MT_INIT);
+	while (true){
+		cout << "Menu:\n1.Send to all\n2.Send to one\n3. Exit\n";
+		int menu;
+		cin >> menu;
+		switch (menu)
+		{
+		case 1: {
+				cout << "Enter your message\n";
+				string str;
+				cin >> str;
+				Message::send(MR_ALL, MT_DATA, str);
+				cout << "Message has sent\n";
+				break;
+		}
+		case 2: {
+				cout << "Enter your message\n";
+				string str;
+				cin >> str;
+				cout << "Enter client's id\n";
+				int toId;
+				cin >> toId;
+				Message m;
+				if (toId == m.clientID) {
+					cout << "You can't send message to yourself\n";
+					break;
+				}
+				else {
+					Message::send(toId, MT_DATA, str);
+					cout << "Message has sent\n";
+					break;
+				}
+		}
+		case 3: {
+				Message m = Message::send(MR_BROKER, MT_EXIT);
+				exit(0);
+				break;
+		}
+		}
+	}
+}
+
+CWinApp theApp;
+
+int main()
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(nullptr);
+
+	if (hModule != nullptr)
+	{
+		// initialize MFC and print and error on failure
+		if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+		{
+			// TODO: code your application's behavior here.
+			wprintf(L"Fatal Error: MFC initialization failed\n");
+			nRetCode = 1;
+		}
+		else
+		{
+			Client();
+		}
+	}
+	else
+	{
+		// TODO: change error code to suit your needs
+		wprintf(L"Fatal Error: GetModuleHandle failed\n");
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
---
+++ SocketServer/SocketServer.cpp
@@ -0,0 +1,168 @@
+// SocketServer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "SocketServer.h"
+#include "Message.h"
+#include "Session.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+void LaunchClient()
+{
+	STARTUPINFO si = { sizeof(si) };
+	PROCESS_INFORMATION pi;
+	CreateProcess(NULL, (LPSTR)"SocketClient.exe", NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+	CloseHandle(pi.hThread);
+	CloseHandle(pi.hProcess);
+}
+
+int maxID = MR_USER;
+map<int, shared_ptr<Session>> sessions;
+
+void checkClients() {
+	int del = 0;
+	for (auto& session : sessions)
+		{
+			std::chrono::duration<double> elapsed_seconds = std::chrono::high_resolution_clock::now() - session.second->lastInteraction;
+			if (elapsed_seconds.count() >= 5)
+			{
+				session.second->lastInteraction = std::chrono::high_resolution_clock::now();
+				del = session.first;
+			}
+		}
+	if (del != 0) {
+		cout << "Client " + to_string(del) + " kicked: too long AFK\n";
+		sessions.erase(del);
+	}
+}
+
+void ProcessClient(SOCKET hSock)
+{
+	//checkClients();
+	CSocket s;
+	s.Attach(hSock);
+	Message m;
+	int code = m.receive(s);
+	//thread check(checkClients, s.Detach());
+	cout << m.header.to << ": " << m.header.from << ": " << m.header.type << ": " << code << endl;
+	switch (code)
+	{
+	case MT_INIT:
+	{
+		auto session = make_shared<Session>(++maxID, m.data, std::chrono::high_resolution_clock::now());
+		sessions[session->id] = session;
+		Message::send(s, session->id, MR_BROKER, MT_INIT);
+		break;
+	}
+	case MT_EXIT:
+	{
+		cout << "Client " + to_string(m.header.from) + " quited\n";
+		sessions.erase(m.header.from);
+		Message::send(s, m.header.from, MR_BROKER, MT_CONFIRM);
+		break;
+	}
+	case MT_GETDATA:
+	{
+		auto iSession = sessions.find(m.header.from);
+
+		if (iSession != sessions.end())
+		{
+				iSession->second->lastInteraction = std::chrono::high_resolution_clock::now();
+				iSession->second->send(s);
+
+		}
+		break;
+	}
+	default:
+	{
+		
+		cout << "MESSAGE SENT\n";
+		Sleep(100);
+		auto iSessionFrom = sessions.find(m.header.from);
+		if (iSessionFrom != sessions.end())
+		{
+			iSessionFrom->second->lastInteraction = std::chrono::high_resolution_clock::now();
+			auto iSessionTo = sessions.find(m.header.to);
+			if (iSessionTo != sessions.end())
+			{
+				iSessionTo->second->add(m);
+				iSessionTo->second->lastInteraction = std::chrono::high_resolution_clock::now();
+			}
+			else if (m.header.to == MR_ALL)
+			{
+				for (auto& [id, session] : sessions)
+				{
+					if (id != m.header.from){
+						session->lastInteraction = std::chrono::high_resolution_clock::now();
+						session->add(m);
+						}
+				}
+			}
+		}
+		break;
+	}
+	}
+}
+
+void Server()
+{
+	AfxSocketInit();
+
+	CSocket Server;
+	Server.Create(12435);
+
+	for (int i = 0; i < 3; ++i)
+	{
+		LaunchClient();
+	}
+	
+	while (true)
+	{
+		if (!Server.Listen())
+			break;
+		CSocket s;
+		Server.Accept(s);
+
+		thread t(ProcessClient, s.Detach());
+		t.detach();
+		thread c(checkClients);
+		c.detach();
+		//checkClients();
+	}
+}
+
+CWinApp theApp;
+
+int main()
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(nullptr);
+
+	if (hModule != nullptr)
+	{
+		// initialize MFC and print and error on failure
+		if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+		{
+			// TODO: code your application's behavior here.
+			wprintf(L"Fatal Error: MFC initialization failed\n");
+			nRetCode = 1;
+		}
+		else
+		{
+			Server();
+		}
+	}
+	else
+	{
+		// TODO: change error code to suit your needs
+		wprintf(L"Fatal Error: GetModuleHandle failed\n");
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
